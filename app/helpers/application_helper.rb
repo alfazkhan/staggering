d5e4f374cebbb7bf19a4ad6fffccc416c080bcf2
AlfazTest <khanalfaz01@gmail.com>
2019-04-14 16:03:53 +0530

    Fixed Projects index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,21 @@ require 'redcarpet/render_strip'
 module ApplicationHelper
     def markdown(text)
         markdown = raw Markdown.new(text).to_html
+    end
 
-        
+    def down(text)
+
+        len=text.length
+        string=""
+        if len < 200
+            markdown(text)
+        else
+            for i in 0...200
+             string+=text[i]
+            end
+            string+="..."
+            return markdown(string)
+        end
+
     end
 end
